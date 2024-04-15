c3d34c96318a537fc7259575bc21374b0e92a0de
dvngshah <heydevang@gmail.com>
2024-04-16 00:11:07 +0530

    added model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -77,7 +77,6 @@ class DataTransformation:
             preprocessing_obj = self.get_data_transformer_obj()
 
             target_column_name = "math score"
-            numerical_columns = ["reading_score", "writing_score"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
 
@@ -12,6 +13,41 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except:
+        pass
+
+
+def evaluate_model(X_train, X_test, y_train, y_test, models):
+
+    print("In Utils")
+
+    try: 
+
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list (models.values())[i]
+
+            print(model)
+            model.fit(X_train, y_train) #Training the model after fitting data
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(y_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+
+            test_model_score = r2_score(X_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+            # print(report)
+
+
+        return report
+
 
     except:
         pass
